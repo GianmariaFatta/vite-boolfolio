bc5fba8ffd0e015eb68adfa1ed4a12bec0c1d1d9
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-03-15 19:48:31 +0100

    add project card - end exercise

---
+++ src/components/projects/Projectcard.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: { project: Object }
+};
+</script>
+
+
+<template>
+    <div class="card my-3 ">
+        <div class="card-header">
+            <h2>{{ project.title }}</h2>
+            <img class='w-25' :src=project.thumb alt="">
+        </div>
+        <div class="card-body">
+            <p>{{ project.description }}</p>
+        </div>
+        <div class="card-footer">
+            <span>{{ project.updated_at }}</span>
+        </div>
+    </div>
+</template>
+
+
+<style></style>
--- src/components/projects/projectList.vue
+++ src/components/projects/projectList.vue
@@ -1,16 +1,19 @@
 <script>
+import ProjectCard from './Projectcard.vue';
 export default {
-    name: 'Projectlists',
-    props: { projects: Array },
+    name: 'Projectlist',
+    components: { ProjectCard },
+    props: { projects: Array }
 }
 </script>
 
 
 <template>
     <section id="projects-list" class="my-4">
-        <ul v-if="projects.length" class="mt-3">
-            <li v-for="project in projects" :key='project.id'>{{ project.title }}</li>
-        </ul>
+        <h1>sucaaaaaaaaaaa</h1>
+        <div v-if="projects.length" class="mt-3">
+            <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
+        </div>
         <h4 v-else class="text-center">Non ci sono post</h4>
     </section>
 </template>
